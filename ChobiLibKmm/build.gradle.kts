f6b9c12da39f2010edc7f01c534155eb774510f6
chobitech <chobitech.g@gmail.com>
2023-08-08 11:34:11 +0900

    2023/08/08 005

--- ChobiLibKmm/build.gradle.kts
+++ ChobiLibKmm/build.gradle.kts
@@ -75,7 +75,7 @@ kotlin {
             register<MavenPublication>("release") {
                 groupId = "com.github.chobitech"
                 artifactId = "ChobiLibKmm"
-                version = "0.0.1"
+                version = "0.0.2"
 
                 afterEvaluate {
                     from(components["release"])
